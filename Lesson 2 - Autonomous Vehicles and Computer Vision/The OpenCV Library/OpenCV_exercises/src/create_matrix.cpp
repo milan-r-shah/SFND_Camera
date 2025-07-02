2da6f743344b3ba01e4200a0a595a9b8a2f94726
Milan R. Shah <mrshah.wpi@gmail.com>
2025-07-02 12:30:56 -0400

    made changes in create_matrix.cpp based on the STUDENT TASK

--- Lesson 2 - Autonomous Vehicles and Computer Vision/The OpenCV Library/OpenCV_exercises/src/create_matrix.cpp
+++ Lesson 2 - Autonomous Vehicles and Computer Vision/The OpenCV Library/OpenCV_exercises/src/create_matrix.cpp
@@ -13,6 +13,9 @@ void createMatrix1() {
     // STUDENT TASK :
     // Create a variable of type cv::Mat* named m3_8u which has three channels with a
     // depth of 8bit per channel. Then, set the first channel to 255 and display the result.
+    cv::Mat m3_8u;
+    m3_8u.create(nrows, ncols, CV_8UC3);  // three-channel matrix with 8bit unsigned elements
+    m3_8u.setTo(cv::Scalar(255, 0, 0));  // set the first channel to 255 (blue in BGR format)
 
     // show result
     std::string windowName = "First steps in OpenCV (m1_8u)";
@@ -22,6 +25,10 @@ void createMatrix1() {
 
     // STUDENT TASK :
     // Display the results from the STUDENT TASK above
+    windowName = "First steps in OpenCV (m3_8u)";
+    cv::namedWindow(windowName, 1);  // create window
+    cv::imshow(windowName, m3_8u);
+    cv::waitKey(0);  // wait for keyboard input before continuing
 }
 
 int main() {
